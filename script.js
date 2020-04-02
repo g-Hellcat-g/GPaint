c4cecf0dc19a07a3ca5f5c6b933bfd316d16578f
g-Hellcat-g <56199766+g-Hellcat-g@users.noreply.github.com>
2020-04-02 12:46:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let canv = document.getElementById('canvas'),
     ctx  = canv.getContext('2d'),
     isMouseDown = false,
-    sizeLineWidth;
+    sizeLineWidth,
+    optionColor,
+    colorP;
     
     function changeSize () {
      sizeLineWidth = parseInt(document.getElementById('sizeLineWidth').value);
@@ -28,9 +30,19 @@ let canv = document.getElementById('canvas'),
     ctx.clearRect(0, 0, canv.width, canv.height );
    }
 
+   function pickColor () {
+       colorP = document.getElementById('selectColor').selectedIndex;
+       optionColor = document.getElementById('selectColor').options;
+       ctx.fillStyle = optionColor[colorP].text;
+       ctx.strokeStyle = optionColor[colorP].text;
+   }
+
+   
    canv.addEventListener('mousemove',function(e) {
        if(isMouseDown) {
+           pickColor();
         ctx.lineWidth = sizeLineWidth;
+        
             ctx.lineTo(e.clientX, e.clientY);
             ctx.stroke();   
             
